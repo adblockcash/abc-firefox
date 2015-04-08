c4bda44cc98fe75724f3b399ecf1d717c23a5106
ThomasHenderson <admin@adblockcash.org>
2015-04-09 00:07:42 +0200

    Options & firstrun views working on firefox

--- chrome/content/lib/helpers.js
+++ chrome/content/lib/helpers.js
@@ -34,35 +34,3 @@ function require(/**String*/ module)
   Services.obs.notifyObservers(result, "adblockcash-require", module);
   return result.exports;
 }
-
-var {Policy} = require("./contentPolicy");
-var {Filter, InvalidFilter, CommentFilter, ActiveFilter, RegExpFilter,
-     BlockingFilter, WhitelistFilter, ElemHideBase, ElemHideFilter, ElemHideException} = require("./filterClasses");
-var {FilterNotifier} = require("./filterNotifier");
-var {FilterStorage, PrivateBrowsing} = require("./filterStorage");
-var {IO} = require("./io");
-var {defaultMatcher, Matcher, CombinedMatcher} = require("./matcher");
-var {Prefs} = require("./prefs");
-var {RequestNotifier} = require("./requestNotifier");
-var {Subscription, SpecialSubscription, RegularSubscription,
-     ExternalSubscription, DownloadableSubscription} = require("./subscriptionClasses");
-var {Synchronizer} = require("./synchronizer");
-var {UI} = require("./ui");
-var {Utils} = require("./utils");
-
-/**
- * Shortcut for document.getElementById(id)
- */
-function E(id)
-{
-  return document.getElementById(id);
-}
-
-/**
- * Split up all labels into the label and access key portions.
- */
-document.addEventListener("DOMContentLoaded", function splitAllLabelsHandler()
-{
-  document.removeEventListener("DOMContentLoaded", splitAllLabelsHandler, false);
-  Utils.splitAllLabels(document);
-}, false);
--- chrome/content/lib/prefsDefaults.js
+++ chrome/content/lib/prefsDefaults.js
@@ -1,7 +1,42 @@
 // For description of these values see http://adblockcash.org/en/preferences
 
+// Prefs imported from abc-chrome (should be the same as in abc-chrome/lib/prefs)
+let defaults = {
+  enabled: true,
+  data_directory: "",
+  patternsbackups: 5,
+  patternsbackupinterval: 24,
+  savestats: false,
+  privateBrowsing: false,
+  subscriptions_fallbackerrors: 5,
+  subscriptions_fallbackurl: "https://adblockplus.org/getSubscription?version=%VERSION%&url=%SUBSCRIPTION%&downloadURL=%URL%&error=%ERROR%&channelStatus=%CHANNELSTATUS%&responseStatus=%RESPONSESTATUS%",
+  subscriptions_autoupdate: true,
+  subscriptions_antiadblockurl: "https://easylist-downloads.adblockplus.org/antiadblockfilters.txt",
+  subscriptions_tracking_title: "EasyPrivacy",
+  subscriptions_tracking_url: "https://easylist-downloads.adblockplus.org/easyprivacy.txt",
+  documentation_link: "https://adblockplus.org/redirect?link=%LINK%&lang=%LANG%",
+  notificationdata: {},
+  notificationurl: "",
+  stats_total: {},
+  stats_by_domain: {},
+  show_statsinicon: true,
+  show_statsinpopup: true,
+  shouldShowBlockElementMenu: true,
+  hidePlaceholders: true,
+  blockedCashableDomains: [],
+  adblockcash_visitor: null,
+  adblockcash_cashableWebsites: null
+};
+
+for (key in defaults) {
+  let value = defaults[key];
+
+  pref("extensions.adblockcash." + key, value);
+}
+
+
+// Only-firefox prefs
 pref("extensions.adblockcash.currentVersion", "0.0");
-pref("extensions.adblockcash.enabled", true);
 pref("extensions.adblockcash.frameobjects", true);
 pref("extensions.adblockcash.fastcollapse", false);
 pref("extensions.adblockcash.showinstatusbar", false);
@@ -15,28 +50,12 @@ pref("extensions.adblockcash.enable_key", "");
 pref("extensions.adblockcash.flash_scrolltoitem", true);
 pref("extensions.adblockcash.previewimages", true);
 pref("extensions.adblockcash.data_directory", "adblockcash");
-pref("extensions.adblockcash.patternsbackups", 5);
-pref("extensions.adblockcash.patternsbackupinterval", 24);
 pref("extensions.adblockcash.whitelistschemes", "about chrome file irc moz-safe-about news resource snews x-jsd addbook cid imap mailbox nntp pop data javascript moz-icon");
-pref("extensions.adblockcash.subscriptions_autoupdate", true);
 pref("extensions.adblockcash.subscriptions_listurl", "https://adblockplus.org/subscriptions2.xml");
-pref("extensions.adblockcash.subscriptions_fallbackurl", "https://adblockplus.org/getSubscription?version=%VERSION%&url=%SUBSCRIPTION%&downloadURL=%URL%&error=%ERROR%&channelStatus=%CHANNELSTATUS%&responseStatus=%RESPONSESTATUS%");
-pref("extensions.adblockcash.subscriptions_fallbackerrors", 5);
-pref("extensions.adblockcash.documentation_link", "https://adblockplus.org/redirect?link=%LINK%&lang=%LANG%");
-pref("extensions.adblockcash.savestats", true);
 pref("extensions.adblockcash.composer_default", 2);
 pref("extensions.adblockcash.clearStatsOnHistoryPurge", true);
 pref("extensions.adblockcash.report_submiturl", false);
 pref("extensions.adblockcash.recentReports", []);
 pref("extensions.adblockcash.hideContributeButton", false);
 pref("extensions.adblockcash.blockableItemsSize", {width: 200, height: 200});
-pref("extensions.adblockcash.notificationurl", "");
-pref("extensions.adblockcash.notificationdata", {});
-pref("extensions.adblockcash.subscriptions_antiadblockurl", "https://easylist-downloads.adblockplus.org/antiadblockfilters.txt");
-pref("extensions.adblockcash.subscriptions_tracking_title", "EasyPrivacy");
-pref("extensions.adblockcash.subscriptions_tracking_url", "https://easylist-downloads.adblockplus.org/easyprivacy.txt");
 pref("extensions.adblockcash.please_kill_startup_performance", false);
-
-pref("extensions.adblockcash.blockedCashableDomains", []);
-pref("extensions.adblockcash.adblockcash_visitor", null);
-pref("extensions.adblockcash.adblockcash_cashableWebsites", null);
